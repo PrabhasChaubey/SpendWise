0f2a399e3623c517e9841bf8d2855d54774815bc
Prabhas Chaubey <prabhaschaubey46@gmail.com>
2026-03-31 16:40:40 +0530

    getall ttransaction controller done

--- backend/src/controllers/transaction.controller.ts
+++ backend/src/controllers/transaction.controller.ts
@@ -2,7 +2,8 @@ import type { Request, Response } from "express";
 import { asyncHandler } from "../middlewares/asyncHandler.middleware.js";
 import { HTTPSTATUS } from "../config/http.config.js";
 import { createTransactionSchema } from "../validators/transaction.validator.js";
-import { createTransactionService } from "../servies/transaction.service.js";
+import { createTransactionService, getAllTransactionService } from "../servies/transaction.service.js";
+import type { TransactionTypeEnum } from "../models/transaction.model.js";
 
 export const createTransactionController = asyncHandler(
   async (req: Request, res: Response) => {
@@ -18,3 +19,31 @@ export const createTransactionController = asyncHandler(
   }
 );
 
+
+export const getAllTransactionController = asyncHandler(
+  async (req: Request, res: Response) => {
+    const userId = req.user?._id;
+
+    const filters: {
+      keyword?: string;
+      type?: keyof typeof TransactionTypeEnum;
+      recurringStatus?: "RECURRING" | "NON_RECURRING";
+    } = {
+      ...(req.query.keyword && { keyword: req.query.keyword as string }),
+      ...(req.query.type && { type: req.query.type as keyof typeof TransactionTypeEnum }),
+      ...(req.query.recurringStatus && { recurringStatus: req.query.recurringStatus as "RECURRING" | "NON_RECURRING" }),
+    };
+
+    const pagination = {
+      pageSize: parseInt(req.query.pageSize as string) || 20,
+      pageNumber: parseInt(req.query.pageNumber as string) || 1,
+    };
+
+    const result = await getAllTransactionService(userId, filters, pagination);
+
+    return res.status(HTTPSTATUS.OK).json({
+      message: "Transaction fetched successfully",
+      ...result,
+    });
+  }
+);
--- backend/src/routes/transaction.route.ts
+++ backend/src/routes/transaction.route.ts
@@ -1,9 +1,11 @@
 import { Router } from "express";
-import { createTransactionController } from "../controllers/transaction.controller.js";
+import { createTransactionController, getAllTransactionController } from "../controllers/transaction.controller.js";
 
 const transactionRoutes = Router();
 
 transactionRoutes.post("/create", createTransactionController);
+
+transactionRoutes.get("/all", getAllTransactionController);
 
 
 export default transactionRoutes;
